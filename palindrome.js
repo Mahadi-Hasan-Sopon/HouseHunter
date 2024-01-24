a06bc9d41edacfa53cb9a57ca28df81526354ee5
Mahadi Hasan <mahadih137@gmail.com>
2024-01-24 11:52:25 +0600

    Palindrome problem solved

---
+++ palindrome.js
@@ -0,0 +1,25 @@
+// Palindrome check using for loop
+function palindrome(str) {
+
+    var len = str.length;
+    var mid = Math.floor(len/2);
+
+    for ( var i = 0; i < mid; i++ ) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+
+// Palindrome check using build in methods
+function isPalindrome(str) {
+  str = str.replace(/\W/g, '').toLowerCase();
+  return (str == str.split('').reverse().join(''));
+}
+
+console.log(isPalindrome("level"));                   // logs 'true'
+console.log(isPalindrome("levels"));                  // logs 'false'
+console.log(isPalindrome("A car, a man, a maraca"));  // logs 'true'
